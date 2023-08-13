c94a9eecb6e4349897442fed3fb64d7ba57237ff
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-08-13 18:49:07 +0100

    Project section

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -3,13 +3,11 @@ interface Props {
   titleNumber: string;
 }
 
-const SectionTitle = ({ title, titleNumber }: Props) => {
-  return (
-    <h2 className='font-titleFont text-2xl font-semibold flex items-center'>
-      <span className='text-base md:text-lg text-textGreen mr-2'>{titleNumber}.</span>{title}
-      <span className='hidden md:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-gray-700 ml-6'></span>
-    </h2>
-  );
-};
+const SectionTitle: React.FC<Props> = ({ title, titleNumber }) => (
+  <h2 className='font-titleFont text-2xl font-semibold flex items-center'>
+    <span className='text-base md:text-lg text-textGreen mr-2'>{titleNumber}.</span>{title}
+    <span className='hidden md:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-gray-700 ml-6'></span>
+  </h2>
+);
 
 export default SectionTitle;
