ea5b581c07a26ba8febad3c00ab346149c65ed93
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-08-15 01:21:23 +0100

    Portfolio V2.0

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,7 @@
 import SectionTitle from "./SectionTitle";
 import { AiFillThunderbolt } from "react-icons/ai";
 import Image from "next/image";
-import  nft  from "../public/assets/images/my-nft.png";
+import {nft}  from "@/public/assets";
 
 
 const About = () => {
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,9 +1,9 @@
 import logo from "./images/logo.png";
-import weatherImg from "./images/Weather.png";
+// import weatherImg from "./images/Weather.png";
 import buriImg from "./images/Buri.png";
 import nft from "./images/my-nft.png";
 import homofy from "./images/homofy.png"
 
 
-export { logo, weatherImg, buriImg, nft,  homofy };
+export { logo, buriImg, nft,  homofy };
 
