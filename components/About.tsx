1f5c3a6b4eb723823452c471e8c454fca5411b6b
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-08-16 02:52:29 +0100

    Portfolio V2.0

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,7 @@
 import SectionTitle from "./SectionTitle";
 import { AiFillThunderbolt } from "react-icons/ai";
 import Image from "next/image";
-import {nft}  from "@/public/assets";
-
+import { nft } from "@/public/assets";
 
 const About = () => {
   return (
@@ -14,67 +13,72 @@ const About = () => {
       <div className="flex flex-col lgl:flex-row gap-16">
         <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
           <p>
-            I am a frontend developer with 3 years of experience. I
-            have a strong foundation in front-end development and am skilled in
-            creating user-friendly and responsive web applications using React
-            and its ecosystem. I have experience working on a variety of
-            projects, including building and maintaining single page
-            applications,{" "}
-            <span className="text-textGreen">integrating with REST APIs</span>,
-            and implementing responsive design principles. I am also proficient
-            in using tools such as Webpack, npm, and Git for development and
-            deployment. In addition to my technical skills, I am a strong
-            communicator and team player.<span className="text-textGreen">
-              {" "}I am able to work effectively with cross-functional teams and
-              am comfortable taking on new challenges
-            </span>{" "}
-            and learning new technologies as needed. I am always looking to
-            improve my skills and stay up-to-date with the latest best practices
-            in web development. I am excited to continue growing as a developer
-            and making meaningful contributions to projects and teams.
+          As a skilled <span className="text-textGreen">Software Engineer</span>, I thrive on transforming innovative
+            concepts into practical solutions. With a deep passion for coding
+            and problem-solving, I possess expertise in designing, developing,
+            and implementing software applications that enhance user
+            experiences. My proficiency spans across multiple programming
+            languages and technologies, allowing me to adapt to dynamic project
+            requirements. I excel in collaborating within cross-functional
+            teams, effectively communicating complex technical concepts to
+            non-technical stakeholders. My commitment to continuous learning and
+            staying abreast of industry trends enables me to contribute to
+            cutting-edge projects. By combining technical prowess with
+            creativity, I contribute to crafting efficient, user-centric
+            software that drives business success.
           </p>
 
           <p>Technologies i work with:</p>
           <ul className="max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6">
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>Javascript
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>React JS
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>Tailwind CSS
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>Typescript
+            </li>
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>Python
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>Next Js
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>HTML5
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>CSS3
             </li>
-            <li  className="flex items-center gap-2">
+            <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
-              </span>C
+              </span>Linux
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>C/C++
             </li>
           </ul>
         </div>
@@ -88,7 +92,7 @@ const About = () => {
               />
             </div>
           </div>
-          </div>
+        </div>
       </div>
     </section>
   );
