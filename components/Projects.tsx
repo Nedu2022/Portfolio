1089e8ccfd9c6c442dd4f492c78c7e601c179bef
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-09-01 23:37:06 +0100

    Updat

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,16 +1,69 @@
 import Image from "next/image";
 import SectionTitle from "./SectionTitle";
-import { buriImg, homofy, weMap } from "@/public/assets";
+import { buriImg, cryptomania, weMap } from "@/public/assets";
 import { TbBrandGithub } from "react-icons/tb";
 import { RxOpenInNewWindow } from "react-icons/rx";
 
-// max-w-containerSmall mx-auto py-10 lgl:py-32 
+// max-w-containerSmall mx-auto py-10 lgl:py-32
 const Projects = () => {
-  return (                            
-    <section id="project" className="max-w-container mx-auto py-10 lgl:px-28 py-32">
+  return (
+    <section
+      id="project"
+      className="max-w-container mx-auto py-10 lgl:px-28 py-32"
+    >
       <SectionTitle title="Projects I have Built" titleNumber="03" />
       <div className="w-full flex flex-col items-center justify-between gap-28 mt-10">
         {/* Project one */}
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row-reverse gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              href="https://cryptomania-hub.vercel.app"
+              target="_blank"
+            >
+              <div>
+                <Image
+                  className="w-full h-full object-contain"
+                  src={cryptomania}
+                  alt="crypto"
+                />
+              </div>
+            </a>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
+              <p className="font-titleFont text-textGreen text-sm tracking-wide">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold">Cryptomania</h3>
+              <p className="bg-[#112240] text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md">
+                This is a cryptocurrency react application with in-depth data
+                about all crypto currencies crypto markets and popular crypto
+                currency news
+              </p>
+              <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+                <li>React JS</li>
+                <li>Redux</li>
+                <li>Rapid API</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+                <a
+                  className="hover:text-textGreen duration-300"
+                  href="https://github.com/Nedu2022/cryptomania"
+                  target="_blank"
+                >
+                  <TbBrandGithub />
+                </a>
+                <a
+                  className="hover:text-textGreen duration-300"
+                  href="https://cryptomania-hub.vercel.app"
+                  target="_blank"
+                >
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
           <div className="flex flex-col xl:flex-row gap-6">
             <a
@@ -30,7 +83,9 @@ const Projects = () => {
               <p className="font-titleFont text-textGreen text-sm tracking-wide">
                 Featured Project
               </p>
-              <h3 className="text-2xl font-bold">Weather Map(Desktop view only 🤪)</h3>
+              <h3 className="text-2xl font-bold">
+                Weather Map(Desktop view only 🤪)
+              </h3>
               <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
                 The Weather map, a dynamic website designed to provide visual
                 representations of various meteorological data and information
@@ -39,8 +94,7 @@ const Projects = () => {
                 conditions as well as forecasts for future time periods, helping
                 to predict weather patterns and trends.
               </p>
-              <ul
-                className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+              <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
                 <li>React Js</li>
                 <li>TypeScript</li>
                 <li>Weather-api</li>
@@ -67,6 +121,8 @@ const Projects = () => {
         </div>
 
         {/* Project two */}
+
+        {/* Project three */}
         <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
           <div className="flex flex-col xl:flex-row-reverse gap-6">
             <a
@@ -111,59 +167,6 @@ const Projects = () => {
                 <a
                   className="hover:text-textGreen duration-300"
                   href="https://buri.netlify.app/"
-                  target="_blank"
-                >
-                  <RxOpenInNewWindow />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Project three */}
-        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
-          <div className="flex flex-col xl:flex-row gap-6">
-            <a
-              className="w-full xl:w-1/2 h-auto relative group"
-              href="https://buri.netlify.app"
-              target="_blank"
-            >
-              <div>
-                <Image
-                  className="w-full h-full object-contain"
-                  src={homofy}
-                  alt="expense"
-                />
-              </div>
-            </a>
-            <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
-              <p className="font-titleFont text-textGreen text-sm tracking-wide">
-                Featured Project
-              </p>
-              <h3 className="text-2xl font-bold">Homofy</h3>
-              <p className="bg-[#112240] text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md">
-                The real estate app is a mobile application designed to provide
-                users with tools and information related to buying, selling,
-                renting, or investing in real estate properties. The app serves
-                as a convenient and accessible platform for individuals, agents,
-                and investors to navigate the real estate market.
-              </p>
-              <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-              </ul>
-              <div className="text-2xl flex gap-4">
-                <a
-                  className="hover:text-textGreen duration-300"
-                  href="https://github.com/Nedu2022/Homofy"
-                  target="_blank"
-                >
-                  <TbBrandGithub />
-                </a>
-                <a
-                  className="hover:text-textGreen duration-300"
-                  href="https://homofy.netlify.app/#"
                   target="_blank"
                 >
                   <RxOpenInNewWindow />
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -2,8 +2,8 @@ import logo from "./images/logo.png";
 import weMap from "./images/we-map.png"
 import buriImg from "./images/Buri.png";
 import nft from "./images/my-nft.png";
-import homofy from "./images/homofy.png";
+import cryptomania from "./images/cryptomania.png";
 
 
-export { logo, buriImg, nft, weMap, homofy };
+export { logo, buriImg, nft, weMap, cryptomania };
 
