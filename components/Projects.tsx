8a8191db6ba11befcc13d12261e3795d6f896079
Nedu2022 <nwabuokeichinedu00@gmail.com>
2024-09-25 09:57:29 +0100

    adding featured project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import SectionTitle from "./SectionTitle";
-import { buriImg, cryptomania, weMap, haap } from "@/public/assets";
+import {
+  buriImg,
+  cryptomania,
+  weMap,
+  haap,
+  borderlands
+} from "@/public/assets";
 import { TbBrandGithub } from "react-icons/tb";
 import { RxOpenInNewWindow } from "react-icons/rx";
 
@@ -13,6 +19,58 @@ const Projects = () => {
     >
       <SectionTitle title="Projects I have Built" titleNumber="03" />
       <div className="w-full flex flex-col items-center justify-between gap-28 mt-10">
+        {/* Project one */}
+        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+          <div className="flex flex-col xl:flex-row-reverse gap-6">
+            <a
+              className="w-full xl:w-1/2 h-auto relative group"
+              href="https://b-watch.vercel.app/"
+              target="_blank"
+            >
+              <div>
+                <Image
+                  className="w-full h-full object-contain"
+                  src={borderlands}
+                  alt="BMovie"
+                />
+              </div>
+            </a>
+            <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
+              <p className="font-titleFont text-textGreen text-sm tracking-wide">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold">BMovie</h3>
+              <p className="bg-[#112240] text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md">
+                A responsive web app built with React and Vite, featuring a hero
+                slider for trending movies and scrollable movie lists.
+                Integrated with an API to fetch and display movie data, the app
+                delivers a fast and smooth user experience across devices.
+              </p>
+              <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+                <li>React JS</li>
+                <li>Tailwind CSS</li>
+                <li>TMDB</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+              <a
+                  className="hover:text-textGreen duration-300"
+                  href="https://github.com/Nedu2022/BMovie"
+                  target="_blank"
+                >
+                  <TbBrandGithub />
+                </a>
+                <a
+                  className="hover:text-textGreen duration-300"
+                  href="https://b-watch.vercel.app/"
+                  target="_blank"
+                >
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* Project two */}
         <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
           <div className="flex flex-col xl:flex-row gap-6">
@@ -58,7 +116,7 @@ const Projects = () => {
           </div>
         </div>
 
-        {/* Project two */}
+        {/* Project three */}
         <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
           <div className="flex flex-col xl:flex-row-reverse gap-6">
             <a
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -3,8 +3,9 @@ import weMap from "./images/we-map.png"
 import buriImg from "./images/Buri.png";
 import nft from "./images/my-nft.png";
 import cryptomania from "./images/cryptomania.png";
-import haap from "./images/Haap.png"
+import haap from "./images/Haap.png";
+import borderlands from "./images/Borderlands.png";
 
 
-export { logo, buriImg, nft, weMap, cryptomania, haap };
+export { logo, buriImg, nft, weMap, cryptomania, haap, borderlands };
 
